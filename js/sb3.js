2124f09ed02e00d31c1ad12f3c78f831604069ad
navneet7393 <56058754+navneet7393@users.noreply.github.com>
2020-05-10 19:54:35 -0700

    Ready for grading

--- js/sb3.js
+++ js/sb3.js
@@ -3,28 +3,57 @@
 
 function verse1() {
     let output = ''
-    // your code here
+    output = `One, two, one, two, three, four </br>
+    Yes I needed some time to get away</br>
+    I needed some peace of mind</br>
+    Some peace of mind that'll stay</br>
+    So I thumbed it, now it's six in L.A.</br>
+    Maybe a Greyhound could be my way </br></br>` 
 
     return output
 }
 
 function verse2() {
     let output = ''
-    // your code here
+    output = `Police and niggers, that's </br>
+    Get outta my way</br>
+    Don't need to buy none of your</br>
+    Gold chains today</br>
+    Now don't need no bracelets</br>
+    Clamped in front of my back</br>
+    Just need my ticket 'till then</br>
+    Won't you cut me some slack</br></br>`
 
     return output
 }
 
 function chorus1() {
     let output = ''
-    // your code here
+    output = `You're one in a million</br>
+    Yeah that's what you are</br>
+    You're one in a million babe</br>
+    You're a shooting star</br>
+    Maybe some day we'll see you</br>
+    Before you make us cry</br>
+    You know we tried to reach you</br>
+    But you were much to high</br>
+    Much too high</br>
+    Much too high</br>
+    Much too high</br></br>`
 
     return output
 }
 
 function chorus2() {
     let output = ''
-    // your code here
+    output = `Immigrants and fagots</br>
+    They make no sense to me</br>
+    They come to our country</br>
+    And think they'll do as they please</br>
+    Like start some mini-Iran</br>
+    Or spread some fucking disease</br>
+    And they talk so many God damn ways</br>
+    It's all Greek to me</br></br>`
 
     return output
 }
@@ -33,7 +62,8 @@ function chorus2() {
 
 function main() {
     let finalOutput = '' // used to build lyrics string
-    // call verse and chorus functions
+    // call verse and chorus functions  
+    finalOutput = verse1() + verse2() + chorus1() + chorus2()
     // each verse/chorus should return text including HTML paragraph tags
 
     // do not modify the statement below
